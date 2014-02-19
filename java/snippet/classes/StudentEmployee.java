f4fdc04781b1a9ecd52f0321501c08041518c16a
stanzheng <dev.stanzheng@gmail.com>
2014-02-19 00:26:32 -0500

    testest

---
+++ java/snippet/classes/StudentEmployee.java
@@ -0,0 +1,14 @@
+public class StudentEmployee extends Student {
+	public String employeeID;
+	public Int salary
+
+	public StudentEmployee(String ID, Int amount)
+	}
+
+	public String getEmployeeID() {
+    return super.toString() + "\t" + somethingnew;
+  }
+  	public Int setEmployeeID() {
+    return super.toString() + "\t" + somethingnew;
+  }
+}
--- java/test/hello.java
+++ java/test/hello.java
@@ -1,7 +1,7 @@
 
 public class hello{
 	//public static void Main(String[] args){
-  public static void main(String[] args){    
+  public static void main(String[] args){
     System.out.println("Hello World");
     System.out.println(sum(3,4));
 	}
