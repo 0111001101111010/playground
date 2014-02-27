ce95705a2d8d111f5aa2f5ed0325c42d109212c9
stanzheng <dev.stanzheng@gmail.com>
2014-02-26 22:48:25 -0500

    eulerize me

--- js-vanilla/euler/1.js
+++ js-vanilla/euler/1.js
@@ -1,6 +1,8 @@
 var _ = require('underscore');
 //var assert = require('chai').assert;
-var assert = require("assert");
+var assert = require('chai').assert;
+//var assert = require("assert");
+
 var sum = 0;
 _.each(
 		 _.range(0,1000),function(num){
@@ -9,17 +11,8 @@ _.each(
 });
 console.log(sum);
 
-/*If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
-
-Find the sum of all the multiples of 3 or 5 below 1000.
-*/
-
-
-describe('Array', function(){
-  describe('#indexOf()', function(){
-    it('should return -1 when the value is not present', function(){
+describe('Test 1', function () {
+      it('sanity', function () {
       assert.equal(sum, 233168);
-    })
-  })
-})
-
+      });
+    });
