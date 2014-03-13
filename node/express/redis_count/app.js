1a5cacf4214eb4bafdddfc87ba3cc256014e9293
stanzheng <dev.stanzheng@gmail.com>
2014-03-13 16:31:39 -0400

    sockets

--- node/express/redis_count/app.js
+++ node/express/redis_count/app.js
@@ -1,18 +1,18 @@
- 
+
 var express = require('express');
 var redis = require('redis');
 var db = redis.createClient();
 var app = express();
- 
+
 // track users online (replace UA string with user id)
- 
+
 app.use(function(req, res, next){
   var ua = req.headers['user-agent'];
   db.zadd('online', Date.now(), ua, next);
 });
- 
+
 // fetch the users online in the last minute
- 
+
 app.use(function(req, res, next){
   var min = 60 * 1000;
   var ago = Date.now() - min;
@@ -22,11 +22,11 @@ app.use(function(req, res, next){
     next();
   });
 });
- 
+
 // route
- 
+
 app.get('/', function(req, res){
   res.send(req.online.length + ' users online');
 });
- 
+console.log("app started on port:3000");
 app.listen(3000);
