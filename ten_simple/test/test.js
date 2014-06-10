1642d7417c44fa61714ca34db8872a7beba5e4fe
stanzheng <dev.stanzheng@gmail.com>
2014-06-10 03:09:48 -0400

    why doea the test pass

--- ten_simple/test/test.js
+++ ten_simple/test/test.js
@@ -1,2 +1,15 @@
-var assert = require('chai').assert();
-var ten = require('../.');
+var assert = require('chai').assert;
+var ten = require('../ten.js');
+
+describe('State', function(){
+  describe('Start', function(){
+        it('should return 10 ', function(){
+          // assert('foo' !== 'bar', 'foo is not bar');
+          // assert(Array.isArray([]), 'empty arrays are arrays');
+          // assert.equal(-1, [1,2,3].indexOf(5));
+          // assert.equal(-1, [1,2,3].indexOf(0));
+          assert(ten,10,'is not value ten');
+          console.log(ten);
+    });
+  });
+});
