d3f1627001a57526be59fababb4155dbc1ee0677
stanzheng <dev.stanzheng@gmail.com>
2014-06-11 01:54:58 -0400

    serious what

--- ten_simple/test/test.js
+++ ten_simple/test/test.js
@@ -6,8 +6,7 @@ describe('Exercise 1', function(){
   describe('A > B', function(){
         it('Given 6 & 5', function(){
           var val = ten.A(5,6);
-          console.log(val);
-          assert.equal(val,4,'6 is larger');
+          assert.equal(val,6,'6 is larger');
         });
   });
   describe('B > A', function(){
