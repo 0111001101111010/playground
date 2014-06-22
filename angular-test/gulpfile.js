f2efb95803bcafe90ba28ec485b4243938e18942
stanzheng <dev.stanzheng@gmail.com>
2014-06-22 12:42:46 -0400

    adding gulp file to run server task

---
+++ angular-test/gulpfile.js
@@ -0,0 +1,9 @@
+// Dev task
+gulp.task('dev', function() {
+  // Start webserver
+  server.listen(serverport);
+  // Start live reload
+  lrserver.listen(livereloadport);
+  // Run the watch task, to keep taps on changes
+  gulp.run('watch');
+});
