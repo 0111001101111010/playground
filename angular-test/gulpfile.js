e00240cff2225fa22961d21d27c6279cf03b1ae5
stanzheng <dev.stanzheng@gmail.com>
2014-06-22 12:45:37 -0400

    adding gulp task

--- angular-test/gulpfile.js
+++ angular-test/gulpfile.js
@@ -1,3 +1,40 @@
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    jshint = require('gulp-jshint'),
+    browserify = require('gulp-browserify'),
+    concat = require('gulp-concat'),
+    clean = require('gulp-clean');
+
+// JSHint task
+gulp.task('lint', function() {
+  gulp.src('./app/scripts/*.js');
+  .pipe(jshint())
+  // You can look into pretty reporters as well, but that's another story
+  .pipe(jshint.reporter('default'));
+});
+
+// Browserify task
+gulp.task('browserify', function() {
+  // Single point of entry (make sure not to src ALL your files, browserify will figure it out for you)
+  gulp.src(['app/scripts/main.js']);
+  .pipe(browserify({
+    insertGlobals: true,
+    debug: true
+  }))
+  // Bundle to a single file
+  .pipe(concat('bundle.js'))
+  // Output it to our dist folder
+  .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('watch', ['lint'], function() {
+  // Watch our scripts
+  gulp.watch(['app/scripts/*.js', 'app/scripts/**/*.js'],[
+    'lint',
+    'browserify'
+  ]);
+});
+
 // Dev task
 gulp.task('dev', function() {
   // Start webserver
@@ -6,4 +43,13 @@ gulp.task('dev', function() {
   lrserver.listen(livereloadport);
   // Run the watch task, to keep taps on changes
   gulp.run('watch');
+});
+
+gulp.task('views', function() {
+  gulp.src('./app/index.html')
+  .pipe(gulp.dest('dist/'));
+
+  gulp.src('./app/views/**/*')
+  .pipe(gulp.dest('dist/views/'))
+  .pipe(refresh(lrserver)); // Tell the lrserver to refresh
 });
