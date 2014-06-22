c6191193087a7b246bb567dc2655cec028c4b518
stanzheng <dev.stanzheng@gmail.com>
2014-06-22 11:42:18 -0400

    init commit

---
+++ angular-test/app/scripts/main/main-ctrl.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('angularTest')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
