75e7306166eeecc9af03d048c29e3d85b0b7ebaf
stanley zheng <dev.stanzheng@gmail.com>
2014-07-04 19:11:37 -0400

    types

---
+++ go/types.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main()  {
+
+  var x string
+  x = "first "
+  //declare and initalize
+  y := 0
+  fmt.Println(x)
+  x = x + "second"
+  fmt.Println(x)
+  //will break, wont allow you
+  fmt.Println(x+y)
+}
