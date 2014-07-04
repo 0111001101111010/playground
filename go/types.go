6523b212183dca2f6b631b16d31452cb9ead1594
stanley zheng <dev.stanzheng@gmail.com>
2014-07-04 19:46:48 -0400

    space to tabs

--- go/types.go
+++ go/types.go
@@ -2,15 +2,15 @@ package main
 
 import "fmt"
 
-func main()  {
+func main() {
 
-  var x string
-  x = "first "
-  //declare and initalize
-  y := 0
-  fmt.Println(x)
-  x = x + "second"
-  fmt.Println(x)
-  //will break, wont allow you
-  fmt.Println(x+y)
+	var x string
+	x = "first "
+	//declare and initalize
+	y := 0
+	fmt.Println(x)
+	x = x + "second"
+	fmt.Println(x)
+	//will break, wont allow you
+	fmt.Println(x + y)
 }
