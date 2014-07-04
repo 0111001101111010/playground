ffb03365b170f3c786278d4aeeebcaa8bd5506ca
stanley zheng <dev.stanzheng@gmail.com>
2014-07-04 19:46:57 -0400

    calculator for principal

---
+++ go/calculator.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	principal := 0.0
+	interest := 0.0
+
+	fmt.Scanf("%f", &principal)
+	fmt.Scanf("%f", &interest)
+	//  output := principal * interest
+
+	fmt.Println(output)
+}
