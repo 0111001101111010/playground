8ba4aa870b71fff0acaea4d0ae490895a1a8b159
stanley zheng <dev.stanzheng@gmail.com>
2014-07-05 01:40:44 -0400

    adding gin example

---
+++ go/practical/gin/gin_rest.go
@@ -0,0 +1,17 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	// Creates a gin router + logger and recovery (crash-free) middlewares
+	r := gin.Default()
+
+	r.GET("/someGet", getting)
+	r.POST("/somePost", posting)
+	r.PUT("/somePut", putting)
+	r.DELETE("/someDelete", deleting)
+	r.PATCH("/somePatch", patching)
+
+	// Listen and server on 0.0.0.0:8080
+	r.Run(":8080")
+}
