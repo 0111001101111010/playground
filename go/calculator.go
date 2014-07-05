cfacf46d529283aaa8c17a8bee935a273f624e1c
stanley zheng <dev.stanzheng@gmail.com>
2014-07-04 20:28:00 -0400

    adding function

--- go/calculator.go
+++ go/calculator.go
@@ -5,10 +5,16 @@ import "fmt"
 func main() {
 	principal := 0.0
 	interest := 0.0
-
+	fmt.Println("Principal?")
 	fmt.Scanf("%f", &principal)
+
+	fmt.Println("Interest? (In percentage format)")
 	fmt.Scanf("%f", &interest)
-	//  output := principal * interest
+	output := principal * percentage(interest)
 
 	fmt.Println(output)
+}
+
+func percentage(input float64) {
+	return 1.0 + input
 }
