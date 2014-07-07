90ef61665ffb6bfbe2eefbde545c5ff672cb83a5
stanley zheng <dev.stanzheng@gmail.com>
2014-07-06 23:36:59 -0400

    counts some stuff

--- exercism/javascript/word-count/word-count.js
+++ exercism/javascript/word-count/word-count.js
@@ -4,15 +4,15 @@ var _ = require('lodash');
 
 function words(string) {
   var count = {};
-  var split = string.split('');
+  var split = string.split(' ');
   _.each(split, function (word) {
-    
-    if (_.contains(count, word)){
-      count.word++;
+    if (count.hasOwnProperty(word)){
+      count[word]++;
     }
     else {
-      count.word = 1;
+      count[word] = 1;
     }
+
   });
   return count;
 }
