2848909cb4f405f52854bc8198994f3b250b62ec
stanley zheng <dev.stanzheng@gmail.com>
2014-07-06 22:53:35 -0400

    sure and whatever

--- exercism/javascript/bob/bob.js
+++ exercism/javascript/bob/bob.js
@@ -1,9 +1,28 @@
 /*jslint node: true */
-
 "use strict";
+
+var _ = require('lodash');
 
 module.exports = Bob;
 
 function Bob() {
-  // body...
+  this.hey = function (phrase) {
+    var string = phrase.split('');
+    if ((string[phrase.length-1]) === '?') {
+      return "Sure.";
+    }
+    else{
+      return "Whatever.";
+    }
+  };
 }
+
+// Bob is a lackadaisical teenager. In conversation, his responses are very limited.
+//
+// Bob answers 'Sure.' if you ask him a question.
+//
+// He answers 'Woah, chill out!' if you yell at him.
+//
+// He says 'Fine. Be that way!' if you address him without actually saying anything.
+//
+// He answers 'Whatever.' to anything else.
