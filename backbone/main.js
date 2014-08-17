3468411534e2df7a6eb3f423c492148e08008219
stanley zheng <dev.stanzheng@gmail.com>
2014-08-17 18:00:39 -0400

    adding Collection

--- backbone/main.js
+++ backbone/main.js
@@ -15,5 +15,21 @@ $(function(){
     }
   });
   //create collection
-  
+  var TodoList = Backbone.Collection.extend({
+    model: Todo,
+    localStorage: new Backbone.LocalStorage("todos-backbone"),
+    done: function () {
+      return this.where({done: true});
+    },
+    remaining: function () {
+      return this.where({done: false});
+    },
+    nextOrder: function () {
+      if (!this.length){
+        return 1;
+      }
+      return this.last().get("order") +1;
+    },
+    comparator: 'order'
+  });
 });
