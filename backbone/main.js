27702f1d320e55f16521ea8672eb1e357d9a7a09
stanley zheng <dev.stanzheng@gmail.com>
2014-08-17 17:48:18 -0400

    kodel interface complete

--- backbone/main.js
+++ backbone/main.js
@@ -1,5 +1,19 @@
 $(function(){
   //I hope lookmai is happy, had to go find local Jquery
-
+    //create Model
+    var Todo = Backbone.Model.extend({
+    defaults: function () {
+      return {
+        title: "blank",
+        order: Todos.nextOrder(),
+        done: false
+      };
+    },
+    //adding function to toggle
+    toggle: function () {
+      this.save({done: !this.get("done")});
+    }
+  });
+  //create collection
   
 });
