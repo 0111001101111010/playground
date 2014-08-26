4c1fc04b0e0a387121c12cfe2c8a345f5ca10797
stanley zheng <dev.stanzheng@gmail.com>
2014-08-26 09:28:28 -0400

    how to sort in reverse order

---
+++ nodeschool/lodash/2.js
@@ -0,0 +1,12 @@
+    // include the Lo-Dash library
+    var _ = require("lodash");
+
+    var worker = function(items) {
+        // do work; return stuff
+        return _.sortBy(items, function (item) {
+        	return -item.quantity;
+        });
+    };
+
+    // export the worker function as a nodejs module
+    module.exports = worker;
