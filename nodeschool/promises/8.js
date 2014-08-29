71b83f0d8955fe3372f232063c83e386dbd20efe
stanley zheng <dev.stanzheng@gmail.com>
2014-08-29 12:00:31 -0400

    simple thing

---
+++ nodeschool/promises/8.js
@@ -0,0 +1,25 @@
+var q = require('q');
+var defer = q.defer();
+
+//docs
+// doStuff()
+// .then(doMoreStuff)
+// .then(null, complainAboutJavascript)
+// .done();
+
+var throwMyGod = function () {
+  return new Error("OH NOES");
+};
+
+var iterate = function (num) {
+  console.log(num);
+  return num +1;
+};
+
+q.fcall(iterate,1)
+ .then(iterate)
+ .then(iterate)
+ .then(iterate)
+ .then(iterate)
+ .then(throwMyGod)
+ .done(console.log);
