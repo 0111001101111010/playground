12635c3555c01e0a217b6dc4c4fad1eb54dabfdd
stanley zheng <dev.stanzheng@gmail.com>
2014-08-29 10:38:23 -0400

    yay i know how to promise :

---
+++ nodeschool/promises/5.js
@@ -0,0 +1,22 @@
+var q = require('q');
+var defer = q.defer();
+
+//docs instructions
+/*
+doSomeSetup()
+.then(function () {
+  return cache.fetchModel(id)
+    ? cache.fetchModel(id)
+    : promisedAjax("users/" + id);
+})
+.then(displayUser)
+.then(null, handleError);
+*/
+
+var attachTitle = function attachTitle (name){
+	return "DR. " + name;
+};
+
+defer.promise.then(attachTitle).then(console.log);
+
+defer.resolve("MAHATTAN");
