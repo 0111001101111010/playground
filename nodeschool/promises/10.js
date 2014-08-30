f06330c2bff35789ef4054565438a1571fc80035
stanley zheng <dev.stanzheng@gmail.com>
2014-08-30 16:44:55 -0400

    adding two promises

--- nodeschool/promises/10.js
+++ nodeschool/promises/10.js
@@ -1,12 +1,26 @@
-var q = require("q");
-var defer = q.defer();
-var io = require("q-io/http");
-var r = require("request");
+/**
+JS ORM
+[
+  "shut",   //status shut|tiny|open -STRING
+  "cap:70; enroll:70;avail:0", //description status - STRING
+  "0",  //avaliable seats - NUMBER
+  "q", // disregard
+  "120g", '//CLASS NAME '
+  "13411", // CRN - NUMBER
+  " LECTURE. STUDENTS MUST ALSO REGISTER FOR A RECITATION SECTION. ", //NOTE - STRING
+  "Introduction to Information Literacy and Research", //DESCRIPTION  // STRING
+  "1500-1615", //TIMES - DATESTRING
+  "TR", //DATES - STRING
+  "MGB 0101", //ROOM - STRING
+  "GUPTA, R", //INSTRUCTOR -
+  "rgupta",  //INSTRUCTOR -
+  "0", // disregard
+  "X", // disregard
+  "", // disregard
+  "dupodd", // disregard NOTE** dupODD and dupEVEN
+  "A", // disregard
+  "N" // disregard
+],
 
-io.read("http://localhost:1337")
-  .then(function (result){
-      return JSON.parse(result);
-       })
-  .then(console.log)
-  .catch(console.err)
-  .done();
+
+**/
