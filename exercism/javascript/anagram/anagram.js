88b4abdd20ddba2f1bb4e27b1ded719a849fefd1
stanley zheng <dev.stanzheng@gmail.com>
2014-10-06 21:27:24 -0400

    this is my bad check

--- exercism/javascript/anagram/anagram.js
+++ exercism/javascript/anagram/anagram.js
@@ -6,8 +6,9 @@ function anagram(word) {
   this.word = word;
   return {
     matches: function (list) {
+      list = Array.isArray(list) ? list : [].slice.call(arguments, 1);
       return _.filter(list, function (candidate) {
-          check(word,candidate);
+          return check(word,candidate);
       });
     }
   };
@@ -16,7 +17,21 @@ function anagram(word) {
 function check(word,candidate) {
   //check if it is the size
   console.log(word, candidate);
+  word = word.toLowerCase();
+  candidate = candidate.toLowerCase();
+
+  // if same word as self then eject
+  if (word === candidate){
+    return false;
+  }
   var diff = _.difference(candidate, word.split(''));
   console.log(diff);
-
+  if(word.length === candidate.length){
+    if (diff.length === 0){
+      return true;
+    }
+  }
+  else {
+    return false;
+  }
 }
