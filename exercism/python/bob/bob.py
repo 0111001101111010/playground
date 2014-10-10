d287f079655028e48896ea0486753a910bfcae26
stanley zheng <dev.stanzheng@gmail.com>
2014-10-09 22:16:36 -0400

    fun with python strings

--- exercism/python/bob/bob.py
+++ exercism/python/bob/bob.py
@@ -1,3 +1,18 @@
-class bob():
-  def hey():
-    return "Whatever"
+def hey(phrase):
+  lastchar = phrase[-1:]
+  if (lastchar == '!'):
+    return 'Woah, chill out!'
+  elif (len(phrase) == 0):
+    return 'Fine. Be that way!'
+  else:
+    return 'whatever'
+
+def yell(phrase):
+  return 'Woah, chill out!'
+
+
+
+if __name__ == "__main__":
+  print "hello"
+  print hey("")
+  print hey("hey!")
