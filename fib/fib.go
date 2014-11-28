f3e2eb6823345b6ea0fac12b297bd45e07112c0f
stanley zheng <dev.stanzheng@gmail.com>
2014-11-28 14:53:08 -0500

    eul1 complete

--- fib/fib.go
+++ fib/fib.go
@@ -5,19 +5,20 @@ package main
 // )
 
 func main() {
-	print(eu1(5))
+	print(eu1(1000))
 }
 
 /*
  * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
  * Find the sum of all the multiples of 3 or 5 below 1000.
  */
-func eu1(num int) bool {
+func eu1(size int) int {
 	sum := 0
-
-	if num%5 == 0 {
-		return true
-	} else {
-		return false
+	for num := 0; num < size; num++ {
+		if (num%5 == 0) || (num%3 == 0) {
+			sum += num
+		}
 	}
+
+	return sum
 }
